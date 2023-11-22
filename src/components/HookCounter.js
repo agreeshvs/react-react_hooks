e8bedd06ca433a79f6e7bbb27111b90347ed1fc1
agreeshvs <agreesh777@gmail.com>
2023-11-22 17:19:47 +0530

    Add files via upload

---
+++ src/components/HookCounter.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+function HookCounter() {
+    const [count,setCount] = useState(0)
+    const incrementCount = () => {
+        setCount(count+1)
+    }
+  return (
+    <div>
+        <button 
+            onClick={ incrementCount}>
+            Count {count}
+        </button>
+    </div>
+  )
+}
+
+export default HookCounter
